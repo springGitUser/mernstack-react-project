dae54b8889a9377667290ef8a46d9dd9ea4c3c6b
springGitUser <springGitUser@gmail.com>
2021-09-26 13:24:20 +0530

    React router implemented commit -5

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,17 +2,20 @@ import FooterComponent from "./components/FooterComponent";
 import HeaderComponent from "./components/HeaderComponent";
 import { Container } from "react-bootstrap";
 import HomeScreen from "./screens/HomeScreen";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import ProductScreen from "./screens/ProductScreen";
 const App = () => {
   return (
-    <>
+    <Router>
       <HeaderComponent />
       <main className="py-3">
         <Container>
-          <HomeScreen />
+          <Route path="/" component={HomeScreen} exact></Route>
+          <Route path="/product/:id" component={ProductScreen}></Route>
         </Container>
       </main>
       <FooterComponent />
-    </>
+    </Router>
   );
 };
 
